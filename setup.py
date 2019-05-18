f96e3a37489f81cecfa0452fd45f007201cd0099
Jeremiah C Leary <jeremiah.c.leary@gmail.com>
2019-05-18 11:14:28 -0500

    Adding hcm script. Adding setup.py

---
+++ setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(
+  name='hcm',
+  version='0.1',
+  description='HDL Component Manager',
+  long_description=readme(),
+  classifiers=[
+      'Development Status :: 2 - Pre-Alpha',
+      'Environment :: Console',
+      'Programming Language :: Python :: 2.7',
+      'Programming Language :: Python :: 3',
+      'Intended Audience :: End Users/Desktop',
+      'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+      'Natural Language :: English',
+      'Operating System :: POSIX :: Linux',
+      'Topic :: System :: Archiving :: Packaging'
+  ],
+  url='https://github.com/jeremiah-c-leary/hdl-component-manager',
+  download_url='https://github.com/jeremiah-c-leary/hdl-component-manager',
+  author='Jeremiah C Leary',
+  author_email='jeremiah.c.leary@gmail.com',
+  license='GNU General Public License',
+  packages=find_packages(),
+  zip_safe=False,
+  test_suite='nose.collector',
+  tests_require=['nose'],
+  keywords=['vhdl', 'verilog', 'packager', 'configuration management'],
+  install_requires=[
+    'PyYAML'
+  ],
+  entry_points={
+    'console_scripts': [
+      'vsg = vsg.__main__:main'
+    ]
+  }
+)
