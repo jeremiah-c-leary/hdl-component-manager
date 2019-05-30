a8ef59ea8209a27a40bc10dbba01b1e21f230a06
Jeremiah C Leary <jeremiah.c.leary@gmail.com>
2019-05-29 19:28:06 -0500

    Updating install tests for new parameter.

--- tests/subcommand/install/test_install.py
+++ tests/subcommand/install/test_install.py
@@ -32,23 +32,23 @@ class testInstallSubcommand(unittest.TestCase):
   def test_install_component_that_exists(self, mocked_function):
 
       self.assertFalse(os.path.isdir('rook'))
-      install('http://svn/my_repo/components', 'rook', '1.0.0')
+      install('http://svn/my_repo/components', 'rook', '1.0.0', None)
       self.assertTrue(os.path.isdir('rook'))
 
-      install('http://svn/my_repo/components', 'rook', '1.1.0')
+      install('http://svn/my_repo/components', 'rook', '1.1.0', None)
       self.assertTrue(os.path.isdir('rook'))
 
-      install('http://svn/my_repo/components', 'rook', '2.0.0')
+      install('http://svn/my_repo/components', 'rook', '2.0.0', None)
       self.assertTrue(os.path.isdir('rook'))
 
       self.assertFalse(os.path.isdir('queen'))
-      install('http://svn/my_repo/components', 'queen', '1.0.0')
+      install('http://svn/my_repo/components', 'queen', '1.0.0', None)
       self.assertTrue(os.path.isdir('queen'))
 
-      install('http://svn/my_repo/components', 'queen', '2.0.0')
+      install('http://svn/my_repo/components', 'queen', '2.0.0', None)
       self.assertTrue(os.path.isdir('queen'))
 
-      install('http://svn/my_repo/components', 'queen', '3.0.0')
+      install('http://svn/my_repo/components', 'queen', '3.0.0', None)
       self.assertTrue(os.path.isdir('queen'))
 
 
@@ -56,15 +56,15 @@ class testInstallSubcommand(unittest.TestCase):
   def test_install_component_that_does_not_exist(self, mocked_function):
 
       self.assertFalse(os.path.isdir('rook'))
-      self.assertRaises(SystemExit, install, 'http://svn/my_repo/components', 'rook', '1.0.1')
+      self.assertRaises(SystemExit, install, 'http://svn/my_repo/components', 'rook', '1.0.1', None)
       self.assertFalse(os.path.isdir('rook'))
 
       self.assertFalse(os.path.isdir('knight'))
-      self.assertRaises(SystemExit, install, 'http://svn/my_repo/components', 'knight', '1.0.0')
+      self.assertRaises(SystemExit, install, 'http://svn/my_repo/components', 'knight', '1.0.0', None)
       self.assertFalse(os.path.isdir('knight'))
 
       self.assertFalse(os.path.isdir('queen'))
-      self.assertRaises(SystemExit, install, 'http://svn/my_repo/components', 'queen', '6.0.0')
+      self.assertRaises(SystemExit, install, 'http://svn/my_repo/components', 'queen', '6.0.0', None)
       self.assertFalse(os.path.isdir('queen'))
 
   @mock.patch('subprocess.check_output', side_effect=mocked_subprocess_check_output)
@@ -72,7 +72,7 @@ class testInstallSubcommand(unittest.TestCase):
   def test_install_component_from_url_environment_variable(self, mocked_function):
 
       self.assertFalse(os.path.isdir('rook'))
-      install(None, 'rook', '1.0.0')
+      install(None, 'rook', '1.0.0', None)
       self.assertTrue(os.path.isdir('rook'))
 
   @mock.patch('subprocess.check_output', side_effect=mocked_subprocess_check_output)
@@ -80,7 +80,7 @@ class testInstallSubcommand(unittest.TestCase):
   def test_install_component_with_no_url(self, mocked_function):
 
       self.assertFalse(os.path.isdir('rook'))
-      self.assertRaises(SystemExit, install, None, 'rook', '1.0.0')
+      self.assertRaises(SystemExit, install, None, 'rook', '1.0.0', None)
       self.assertFalse(os.path.isdir('rook'))
 
   @mock.patch('subprocess.check_output', side_effect=mocked_subprocess_check_output)
@@ -88,5 +88,5 @@ class testInstallSubcommand(unittest.TestCase):
   def test_install_component_with_multiple_environment_variables(self, mocked_function):
 
       self.assertFalse(os.path.isdir('rook'))
-      self.assertRaises(SystemExit, install, None, 'rook', '1.0.0')
+      self.assertRaises(SystemExit, install, None, 'rook', '1.0.0', None)
       self.assertFalse(os.path.isdir('rook'))
